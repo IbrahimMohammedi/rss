e8dc5a37ecda7f11318ac86c1c3b360ae8a5ba98
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-09-28 12:15:34 +0100

    package fixed port error, ignored vendor although may be i shouldnt

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found")
