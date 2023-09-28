62ecbb9e7a0f4d32d2e70acb17e0051abb81a598
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-09-28 12:25:57 +0100

    set up the router

--- main.go
+++ main.go
@@ -5,15 +5,32 @@ import (
 	"log"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// loading the .env in our current env
 	godotenv.Load(".env")
-
+	//creating the port var by setting equal to our port from .env
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found")
 	}
-	fmt.Println("Port: ", portString)
+
+	//setting up the router
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Port listening to port %v", portString)
+	err := srv.ListenAndServe(){
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
