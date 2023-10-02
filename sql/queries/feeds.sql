db1026a36be8402ea2a7c37d62f889aed5ca91bb
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-10-02 22:50:52 +0100

    sqlc generate

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -3,6 +3,6 @@ INSERT INTO feeds (id, created_at,updated_at, name, url, user_id)
 VALUES ($1,$2,$3,$4,$5,$6)
 RETURNING *;
 
--- name: CreateFeed :many
+-- name: GetFeed :many
 SELECT * FROM feeds;
 
